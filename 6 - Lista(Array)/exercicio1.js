dbac6b72f64429e7f9b84ff2b112da9ac3c987e1
Sara Moreira <moreirasaragds@gmail.com>
2026-04-22 08:05:49 -0300

    Delete 6 - Lista(Array) directory

--- 6 - Lista(Array)/exercicio1.js
+++
@@ -1,9 +0,0 @@
-/* 1) Crie um programa que dado um número imprima a sua tabuada. */
-
-const numero = 2
-
-for (i = 1; i <= 10; i++) {
-    tabuada = numero * i;
-    console.log(tabuada)
-
-}
--- 6 - Lista(Array)/exercicio6.js
+++
@@ -1,7 +0,0 @@
-/* 6) Dado uma lista com as notas tiradas pelos alunos. imprima a maior nota.
-   Ex de lista de notas = [2, 7, 3, 8, 10, 4] */
-
-const notas = [2, 7, 3, 8, 10, 4];
-
-maiorNota = Math.max(...notas);
-console.log('Maior nota: ' + maiorNota)
