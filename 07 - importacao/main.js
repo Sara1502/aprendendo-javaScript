cdcd65ee46f85d94ae66a20e5909823f6466f0d9
Sara Moreira <moreirasaragds@gmail.com>
2026-04-22 16:06:27 -0300

    Create main.js

---
+++ 07 - importacao/main.js
@@ -0,0 +1,10 @@
+/* 1) Uma sala contem 5 alunos e pra cada aluno foi sorteado um numero de 1 = 100. Faça um prograrma que receba os 
+5 numeros sorteados para os alunos e mostre o maior numero sorteado.
+  Dados de entrada: 5, 50, 10, 98, 23  Saida: 98 */
+
+
+const { gets, print } = require('./funcoes-auxiliares')
+
+
+
+print(gets());
