5795b98d94d4b216e02f69ce98262e1c5a814046
Sara Moreira <moreirasaragds@gmail.com>
2026-04-24 08:56:39 -0300

    Create funcoes-auxiliares-ex1.js

---
+++ 08-Exercicios/funcoes-auxiliares-ex1.js
@@ -0,0 +1,27 @@
+/* 1) Faça um programa que receba a média de um aluno.
+Caso a média seja < 5 imprima "Reprovado"
+Caso a média seja >= 5 e < 7 imprima "Recuperação"
+Caso a média seja >= 7 imprima "Aprovado"
+
+    Exemplo:
+        Entrada:
+            5.5
+        Saída:
+            Recuperação */
+
+
+const entradas = [5.5];
+let i = 0;
+
+function gets() {
+    const valor = entradas[i];
+    i++;
+    return valor;
+}
+
+
+function print(texto) {
+    console.log(texto)
+}
+
+module.exports = { gets, print }
